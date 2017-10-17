94928495803a69712c069c134810f673ef63c684
nicholas-j-b <nicholas.j.brooking@gmail.com>
2017-10-17 19:49:32 +0200

    update for class presentation

--- 1b.R
+++ 1b.R
@@ -1,36 +1,64 @@
+
+# Copyright (C) 2017 Nicholas Brooking
+# 
+# 1b.R is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# 1b.R is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+#small script to address the problem of finding the geometric median
+#generates random points with x value and y value each between 0 and 1
+#calculates the mean of the x and y values separately and uses that as guess
+#builds a rasta over the [0,1]^2 region and calculates sum of distances to all points for each intersection
+
+
+
 library(plotly)
 
-set.seed(7)
+#setup board
+numPoints <- 8
+xVals <- runif(numPoints, 0, 1)
+yVals <- runif(numPoints, 0, 1)
 
-d1 <- runif(10, 0, 1)
-d2 <- runif(10, 0, 1)
+#calculate guess
+xMean <- mean(xVals)
+yMean <- mean(yVals)
 
-p1 <- mean(d1)
-p2 <- mean(d2)
-
+#setup rasta
+#devDist is how far either side of middle to look at
 matSize <- 121
-devDist <- .3
+devDist <- .5
+matx <- seq(from = .5 - devDist, to = .5 + devDist, length.out = matSize)
+maty <- seq(from = .5 - devDist, to = .5 + devDist, length.out = matSize)
+zVals <- matrix(0, nrow = matSize, ncol = matSize)
 
-p1 <- seq(from = (p1 - devDist), to = (p1 + devDist), length.out = matSize)
-p2 <- seq(from = (p2 - devDist), to = (p2 + devDist), length.out = matSize)
-
-ymat <- matrix(0, nrow = matSize, ncol = matSize)
-
+#populate matrix of z values
 for(i in 1:matSize){
   for(j in 1:matSize){
-    ymat[i,j] <- sum(sqrt((p1[i] - d1)**2 + (p2[j] - d2)**2))
+    zVals[i,j] <- (sum(sqrt((matx[i] - xVals)**2 + (maty[j] - yVals)**2)))
   }
 }
 
-inds <- which(ymat == min(ymat), arr.ind = TRUE)
-mean(d1)
-p1[inds[1]]
-mean(d2)
-p2[inds[2]]
+#xy coords for
+inds <- which(zVals == min(zVals), arr.ind = TRUE)
 
+#measure accuracy of guess
+sqrt((xMean - matx[inds[1]])**2 + (yMean - maty[inds[2]])**2)
 
-
-p <- plot_ly(z = ymat) %>% add_surface()
+r <- plot(xVals, yVals, xlim = c(0, 1), ylim = c(0, 1))
+p <- plot_ly(x = matx, y = maty, z = zVals)
+p <- p %>% add_surface()
+q <- plot_ly(z = zVals, x = matx, y = maty, type = "heatmap")
 
 p
 
