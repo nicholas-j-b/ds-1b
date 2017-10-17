170cfd072ed2c5198d1be5f23eae2e4c91116ec8
nicholas-j-b <nicholas.j.brooking@gmail.com>
2017-10-17 20:41:50 +0200

    adjustments and explanatory comments

--- 1b.R
+++ 1b.R
@@ -34,35 +34,74 @@ yVals <- runif(numPoints, 0, 1)
 xMean <- mean(xVals)
 yMean <- mean(yVals)
 
+#constants
+matSize <- 61
+
 #setup rasta
-#devDist is how far either side of middle to look at
-matSize <- 121
-devDist <- .5
-matx <- seq(from = .5 - devDist, to = .5 + devDist, length.out = matSize)
-maty <- seq(from = .5 - devDist, to = .5 + devDist, length.out = matSize)
+matx <- seq(from = 0, to = 1, length.out = matSize)
+maty <- seq(from = 0, to = 1, length.out = matSize)
 zVals <- matrix(0, nrow = matSize, ncol = matSize)
 
 #populate matrix of z values
 for(i in 1:matSize){
   for(j in 1:matSize){
-    zVals[i,j] <- (sum(sqrt((matx[i] - xVals)**2 + (maty[j] - yVals)**2)))
+    for(k in 1:numPoints){
+      zVals[i,j] <- zVals[i,j] + (sqrt((matx[i] - xVals[k])**2 + (maty[j] - yVals[k])**2))
+    }
   }
 }
 
 #xy coords for
-inds <- which(zVals == min(zVals), arr.ind = TRUE)
+minXY <- which(zVals == min(zVals), arr.ind = TRUE)
+print(matx[minXY[1,1]])
+print(maty[minXY[1,2]])
 
-#measure accuracy of guess
-sqrt((xMean - matx[inds[1]])**2 + (yMean - maty[inds[2]])**2)
-
-r <- plot(xVals, yVals, xlim = c(0, 1), ylim = c(0, 1))
+#prepare surface graph before iterations begin
 p <- plot_ly(x = matx, y = maty, z = zVals)
 p <- p %>% add_surface()
+
+
+h <- 1
+while(TRUE){
+  #setup rasta
+  matx <- seq(from = matx[minXY[1,1]] - (1 / (2^h)), to = matx[minXY[1,1]] + (1 / (2^h)), length.out = matSize)
+  maty <- seq(from = maty[minXY[1,2]] - (1 / (2^h)), to = maty[minXY[1,2]] + (1 / (2^h)), length.out = matSize)
+  zVals <- matrix(0, nrow = matSize, ncol = matSize)
+  
+  #populate matrix of z values
+  for(i in 1:matSize){
+    for(j in 1:matSize){
+      for(k in 1:numPoints){
+        zVals[i,j] <- zVals[i,j] + (sqrt((matx[i] - xVals[k])**2 + (maty[j] - yVals[k])**2))
+      }
+    }
+  }
+  
+  tempXY <- which(zVals == min(zVals), arr.ind = TRUE)
+  if(tempXY[1,1] == minXY[1,1] & tempXY[1,2] == minXY[1,2]){
+    break
+  }
+  minXY <- tempXY
+  h <- h + 1
+}
+
+#print data
+cat("number of iterations: ", h)
+cat("final solution: (", matx[minXY[1,1]], maty[minXY[1,2]], ")")
+
 q <- plot_ly(z = zVals, x = matx, y = maty, type = "heatmap")
 
+
+#measure accuracy of guess
+sqrt((xMean - matx[minXY[1]])**2 + (yMean - maty[minXY[2]])**2)
+
+#graph points
+r <- plot(xVals, yVals, xlim = c(0, 1), ylim = c(0, 1))
+r <- points(matx[minXY[1,1]], maty[minXY[1,2]], col = "red")
+
+#show graph
+#type q into console for zoomed in heatmap or p for 3d surface
 p
-
-
 
 
 
